431c7b67a82be79159817b76c39c03c683a11ba8
Qiushan Zhao <aflmjc626@Gmail.com>
2018-01-21 12:11:34 -0800

    first commit

---
+++ src/main/java/resource/RecordRest.java
@@ -0,0 +1,30 @@
+package resource;
+
+import database.DBUtil;
+import model.Record;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Qiushan on 2018/1/17.
+ */
+@Path("record")
+public class RecordRest {
+    @GET
+    @Path("{rid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRecordByRid(@PathParam("rid") Integer rid){
+        Record record = DBUtil.getRecordById(rid);
+        return Response.
+                ok(record, MediaType.APPLICATION_JSON).
+                build();
+    }
+
+
+
+}
